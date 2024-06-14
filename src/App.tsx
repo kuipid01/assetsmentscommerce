9286bd68e1782daf19fb90065180dba9f6208eac
kuipid01 <kuipid01@gmnail.com>
2024-06-14 19:57:50 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import {
-  createBrowserRouter,
-  Outlet,
-  RouterProvider,
-  ScrollRestoration,
-  // useLocation,
-} from "react-router-dom";
-import { useEffect } from "react";
-// import { scroll } from "./animation";
-import Home from "./pages/Home";
-// import { Footer, Navbar } from "./components/global";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const Root = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      {/* <Navbar /> */}
-      <ScrollRestoration />
-      <Outlet />
-      {/* <Footer /> */}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
-};
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      // {
-      //   path: "*",
-      //   element: <NotFound />,
-      // },
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
-  },
-]);
-
-export default function App() {
-  useEffect(() => {
-    scroll();
-  }, []);
-  return <RouterProvider router={router} />;
+  )
 }
+
+export default App
